9b93ead46d31a7ac4784cd66ba79edcc144a09a2
rubish <rubish.gupta@razorpay.com>
2021-01-22 08:36:36 +0530

    minor refactoring

--- src/api/websiteVerificationAPI.js
+++ src/api/websiteVerificationAPI.js
@@ -1,13 +1,17 @@
 import { AsyncRouter } from 'express-async-router';
+import ld from 'lodash';
 
 import CreateWebsiteVerificationRequest from '../commands/CreateWebsiteVerificationRequest.js';
 
 const router = AsyncRouter();
 
 router.post('/', async (req, res) => {
-  // throw new Error('test error');
   const command = new CreateWebsiteVerificationRequest(req.body);
-  res.json(await command.execute());
+  const verificationEntity = await command.execute();
+
+  res.json(
+    ld.pick(verificationEntity, ['_id', 'url', 'createdAt', 'updatedAt'])
+  );
 });
 
 export default router;
--- src/worker.js
+++ src/worker.js
@@ -1,11 +1,12 @@
 import _ from './common/preInitSetup.js';
 
-import queueManager from './events/core/queueManager.js';
-import BackgroundEvent from './events/core/BackgroundEvent.js';
-
 // load up all the events to be registered successfully
 import __ from './events/index.js';
 
+import queueManager from './events/core/queueManager.js';
+import BackgroundEvent from './events/core/BackgroundEvent.js';
+
 queueManager.startConsuming((jobData) => BackgroundEvent.process(jobData));
 
+// eslint-disable-next-line no-console
 console.log('Starting consuming jobs');
